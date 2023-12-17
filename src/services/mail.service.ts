bfc1b2cda2af05d13e416a077b94a4efa9d2953f
fjpimienta <fjpimienta@gmail.com>
2023-12-17 14:36:42 -0600

    API: Fix Configuracion de correos.

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -1,23 +1,28 @@
-import { transport } from '../config/mailer';
+import transporter from '../config/mailer';
 import { IMailOptions } from '../interfaces/email.interface';
+import { SentMessageInfo } from 'nodemailer';
 
 class MailService {
-  send(mail: IMailOptions) {
+  send(mail: IMailOptions): Promise<{ status: boolean; message: string; mail?: IMailOptions }> {
     return new Promise((resolve, reject) => {
-      transport.sendMail({
-        from: '"✅ 🛒DARU Shop ✉ ✅" <marketplace@daru.mx>',     // sender address
-        to: mail.to,                                              // list of receivers
-        subject: mail.subject,                                    // Subject line
-        html: mail.html,                                          // html body
-      }, (error, _) => {
-        (error) ? reject({
-          status: false,
-          message: error
-        }) : resolve({
-          status: true,
-          message: '🛒DARU Shop ✉ Email enviado correctamente a ' + mail.to,
-          mail
-        });
+      transporter.sendMail({
+        from: '"DARU Shop - " <marketplace@daru.mx>',
+        to: mail.to,
+        subject: mail.subject,
+        html: mail.html,
+      }, (error: Error | null, info: SentMessageInfo) => {
+        if (error) {
+          reject({
+            status: false,
+            message: error.message,
+          });
+        } else {
+          resolve({
+            status: true,
+            message: `DARU Shop - Email enviado correctamente a ${mail.to}`,
+            mail,
+          });
+        }
       });
     });
   }

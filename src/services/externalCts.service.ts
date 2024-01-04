53d32ef611497fe7013e0e3ecf7e78f2405fa872
fjpimienta <fjpimienta@gmail.com>
2024-01-03 18:42:45 -0600

    API: External CT - Depuracion.

--- src/services/externalCts.service.ts
+++ src/services/externalCts.service.ts
@@ -437,7 +437,6 @@ class ExternalCtsService extends ResolversOperationsService {
   async downloadFileFromFTP() {
     const client = new Client();
     client.ftp.verbose = true; // Activa para ver información detallada en la consola
-
     try {
       const accessOptions: AccessOptions = {
         host: '216.70.82.104',
@@ -445,13 +444,9 @@ class ExternalCtsService extends ResolversOperationsService {
         password: 'PtZ9hAXJnkJInZXReEwN',
       };
       await client.access(accessOptions);
-
       const remoteFilePath = '/catalogo_xml/productos.json';
       const localFilePath = './uploads/files/productos.json';
       await client.downloadTo(localFilePath, remoteFilePath);
-
-      console.log('Archivo descargado exitosamente');
-
       // Leer y enviar el json.
       const fileContent = fs.readFileSync(localFilePath, 'utf-8');
       const jsonData = JSON.parse(fileContent);
@@ -475,7 +470,6 @@ class ExternalCtsService extends ResolversOperationsService {
   async downloadHPFileFromFTP() {
     const client = new Client();
     client.ftp.verbose = true; // Activa para ver información detallada en la consola
-
     try {
       const accessOptions: AccessOptions = {
         host: '216.70.82.104',
@@ -484,23 +478,15 @@ class ExternalCtsService extends ResolversOperationsService {
       };
       client.ftp.socket.setTimeout(30000);
       await client.access(accessOptions);
-
       const remoteFilePath = '/catalogo_xml/productos_especiales_VHA2391.xml';
       const localFilePath = './uploads/files/productos_especiales_VHA2391.xml';
       await client.downloadTo(localFilePath, remoteFilePath);
-
-      console.log('Archivo descargado exitosamente');
-
       // Leer y enviar el json.
       const fileContent = fs.readFileSync(localFilePath, 'utf-8');
-      // console.log('fileContent: ', fileContent);
-
-      // const jsonData = JSON.parse(fileContent);
       const data = await this.parseXmlToJson(fileContent, 'productos_especiales_VHA2391.xml');
-      // console.log('data: ', data);
       return await {
         status: true,
-        mesage: 'Se ha descargado correctamente el json.',
+        mesage: 'Se ha descargado correctamente el xml.',
         data
       };
     } catch (error) {
@@ -522,52 +508,7 @@ class ExternalCtsService extends ResolversOperationsService {
       const result = await xml2js.parseStringPromise(xml, { explicitArray: false });
       switch (catalog) {
         case 'productos_especiales_VHA2391.xml':
-          return result.Articulo.Producto;
-        case 'grupos.xml':
-          if (result && result.grupos && result.grupos.grupo) {
-            const grupos = Array.isArray(result.grupos.grupo)
-              ? result.grupos.grupo.map((item: any) => ({ grupo: item.trim() }))
-              : [{ grupo: result.grupos.grupo.trim() }];
-            return grupos;
-          } else {
-            return [];
-          }
-        case 'soluciones.xml':
-          return result.soluciones.solucion;
-        case 'sucursales.xml':
-          return result.sucursales.sucursal;
-        case 'paqueteria.xml':
-          return result.paqueterias.paqueteria;
-        case 'lista_precios.xml':
-          return result.articulos.item;
-        case 'ListaPedidos':
-          pedidosXml = result['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:ListaPedidosResponse']['pedidos'];
-          pedidosXmlContent = pedidosXml._;
-          if (typeof pedidosXmlContent === 'string') {
-            const pedidosResult = await xml2js.parseStringPromise(pedidosXmlContent, { explicitArray: false });
-            return pedidosResult.PEDIDOS.pedido;
-          } else {
-            throw new Error('El contenido XML no es válido');
-          }
-        case 'ConsultaPedido':
-          pedidosXml = result['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:ConsultaPedidoResponse']['pedido'];
-          pedidosXmlContent = pedidosXml._;
-          if (typeof pedidosXmlContent === 'string') {
-            const pedidosResult = await xml2js.parseStringPromise(pedidosXmlContent, { explicitArray: false });
-            return pedidosResult.PEDIDO;
-          } else {
-            throw new Error('El contenido XML no es válido');
-          }
-        case 'PedidoWeb':
-          pedidosXmlContent = result['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:PedidoWebResponse'];
-          const error = pedidosXmlContent['error']?._ || null;
-          const estado = pedidosXmlContent['estado']?._ || null;
-          const pedido = pedidosXmlContent['pedido']?._ || null;
-          const total = pedidosXmlContent['total']?._ || null;
-          const agentemail = pedidosXmlContent['agentemail']?._ || null;
-          const almacenmail = pedidosXmlContent['almacenmail']?._ || null;
-          return { error, estado, pedido, total, agentemail, almacenmail };
-
+          return result.Articulo.Producto;       
         default:
           throw new Error('Catálogo no válido');
       }

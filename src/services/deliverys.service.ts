dae60b0bcca09e1703d400dc8d108adae95219bd
fjpimienta <fjpimienta@gmail.com>
2024-02-11 14:06:53 -0600

    API: ExternatlCt - Debug insert/update.

--- src/services/deliverys.service.ts
+++ src/services/deliverys.service.ts
@@ -210,10 +210,16 @@ class DeliverysService extends ResolversOperationsService {
             case 'ct':
               const ordersCt = await this.setOrder(id, delivery, warehouse);
               console.log('ordersCt: ', ordersCt);
-              const orderCtResponse = await this.EfectuarPedidos(supplier, ordersCt, context)
-                .then(async (result) => {
-                  return await result;
-                });
+              // const orderCtResponse = await this.EfectuarPedidos(supplier, ordersCt, context)
+              //   .then(async (result) => {
+              //     return await result;
+              //   });
+              const orderCtResponse = {
+                estatus: 'Good',
+                pedidoWeb: 'NA',
+                errores: [{ errorMessage: 'Forzado' }],
+                delivery: null
+              }
               ordersCt.orderCtResponse = orderCtResponse;
               // Confirmar pedido
               const orderCtConfirm: IOrderCtConfirm = { folio: orderCtResponse.pedidoWeb };
@@ -276,7 +282,8 @@ class DeliverysService extends ResolversOperationsService {
 
       const filter = { id: id.toString() };
 
-      const resultUpdate = await this.updateForce(this.collection, filter, deliveryUpdate, 'Pedido');
+      // const resultUpdate = await this.updateForce(this.collection, filter, deliveryUpdate, 'Pedido');
+      const resultUpdate = await this.insert(context);
       if (resultUpdate && resultUpdate.status) {
         // Si se guarda el envio, inactivar el cupon.
         if (delivery && delivery?.user) {
@@ -297,7 +304,11 @@ class DeliverysService extends ResolversOperationsService {
           delivery: deliveryUpdate
         }
       }
-
+      return {
+        status: false,
+        mesage: 'Problemas con el cargo, verificar datos.',
+        delivery: null
+      };
     }
     return {
       status: false,

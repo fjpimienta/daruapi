a69175bf0a58cf5961eec163b7d4393db1f718c5
fjpimienta <fjpimienta@gmail.com>
2024-02-23 07:11:25 -0600

    API: External Ingram - Fix Build

--- src/resolvers/query/suppliers/_Ingram.ts
+++ src/resolvers/query/suppliers/_Ingram.ts
@@ -21,9 +21,9 @@ const resolversIngramQuery: IResolvers = {
     async catalogIngrams(_, variables, context) {
       return new ExternalIngramService(_, variables, context).getCatalogIngrams();
     },
-    async orderIngram(_, variables, context) {
-      return new ExternalIngramService(_, variables, context).setOrderIngram(variables);
-    }
+    // async orderIngram(_, variables, context) {
+    //   return new ExternalIngramService(_, variables, context).setOrderIngram(variables);
+    // }
   }
 };
 

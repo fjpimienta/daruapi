41bbb8b54e8ac296728d9706c546b590905d0102
fjpimienta <fjpimienta@gmail.com>
2024-07-31 11:30:40 -0600

    API: Import Product - Mejora update y new product.

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -516,7 +516,7 @@ class ProductsService extends ResolversOperationsService {
       console.log('products.length: ', products.length);
 
       let bulkOperations = [];
-      let i = id;
+      let nextId = id; // Inicializamos nextId con el id recuperado
 
       const newProductPartNumbers = new Set(products.map(product => product.partnumber));
 
@@ -549,17 +549,35 @@ class ProductsService extends ResolversOperationsService {
               product.sm_pictures = urlsDeImagenes.map((urlImage: string) => ({ width: '300', height: '300', url: urlImage }));
             }
           }
+
+          // No modificar el id para productos existentes
+          const existingProduct = existingProductsMap.get(product.partnumber);
+          if (existingProduct) {
+            product.id = existingProduct.id;
+          }
         }
 
-        const productC = await this.categorizarProductos(product, i, firstsProducts);
+        // Asignar un nuevo id solo si el producto es nuevo
+        if (!existingProductsMap.has(product.partnumber)) {
+          product.id = nextId.toString();
+          nextId += 1; // Incrementamos el nextId para el siguiente nuevo producto
+        }
+
+        const productC = await this.categorizarProductos(product, nextId, firstsProducts);
+
         productC.sheetJson = '';
         productsAdd.push(productC);
-        i += 1;
+
+        // Combinamos los updates en un solo objeto
+        const updateData = {
+          ...productC,
+          active: true
+        };
 
         bulkOperations.push({
           updateOne: {
             filter: { partnumber: product.partnumber, 'suppliersProd.idProveedor': idProveedor },
-            update: { $set: productC },
+            update: { $set: updateData },
             upsert: true
           }
         });
@@ -595,8 +613,10 @@ class ProductsService extends ResolversOperationsService {
   }
 
   async categorizarProductos(product: IProduct, i: number, firstsProducts: boolean) {
-    // Asignar el id siempre
-    product.id = i.toString();
+    // Asignar el id solo si es un nuevo producto y no tiene uno
+    if (!product.id) {
+      product.id = i.toString();
+    }
 
     // Clasificar Categorias y Subcategorias
     if (product.subCategory && product.subCategory.length > 0) {

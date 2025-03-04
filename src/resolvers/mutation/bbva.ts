62c2e1451282ae38e9b895983a7340c2cca22bb5
Francisco Jesus Perez Pimienta <fjpimienta@gmail.com>
2025-03-04 09:47:03 -0600

    Fixes - BBVA Mutatio & Quiery

--- src/resolvers/mutation/bbva.ts
+++ src/resolvers/mutation/bbva.ts
@@ -1,11 +1,6 @@
-import BBVAService from '../services/bbva.service';
+import BBVAService from "../../services/bbva.service";
 
 const bbvaResolvers = {
-  Query: {
-    payment: async (_source: any, { id }: { id: string }, { dataSources }: { dataSources: { bbvaService: BBVAService } }) => {
-      return await dataSources.bbvaService.getPayment(id);
-    },
-  },
   Mutation: {
     createPayment: async (_source: any, { input }: { input: any }, { dataSources }: { dataSources: { bbvaService: BBVAService } }) => {
       return await dataSources.bbvaService.createPayment(input);
